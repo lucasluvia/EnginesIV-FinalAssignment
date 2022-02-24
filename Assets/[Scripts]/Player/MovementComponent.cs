d5c7629cd7ac45f56facd9022aba16dbfd5bd6bb
lucasdunster <60508486+lucasluvia@users.noreply.github.com>
2022-02-23 19:20:30 -0500

    Initial commit

---
+++ Assets/[Scripts]/Player/MovementComponent.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovementComponent : MonoBehaviour
+{
+    [SerializeField] float walkSpeed = 5.0f;
+    [SerializeField] float runSpeed = 10.0f;
+    [SerializeField] float jumpForce = 5.0f;
+
+    // Components
+    private PlayerController playerController;
+    Rigidbody rigidbody;
+    Animator playerAnimator;
+    public GameObject followTarget;
+
+    // References
+    Vector2 inputVector = Vector2.zero;
+    Vector3 moveDirection = Vector3.zero;
+    Vector2 lookInput = Vector3.zero;
+
+    public float aimSensitivity = 1.0f;
+
+    // Animator Hashes
+    public readonly int movementXHash = Animator.StringToHash("MovementX");
+    public readonly int movementYHash = Animator.StringToHash("MovementY");
+    public readonly int isJumpingHash = Animator.StringToHash("IsJumping");
+    public readonly int isRunningHash = Animator.StringToHash("IsRunning");
+
+    private void Awake()
+    {
+        playerController = GetComponent<PlayerController>();
+        rigidbody = GetComponent<Rigidbody>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        //aimsing,looking
+        followTarget.transform.rotation *= Quaternion.AngleAxis(lookInput.x * aimSensitivity, Vector3.up);
+        followTarget.transform.rotation *= Quaternion.AngleAxis(lookInput.y * aimSensitivity, Vector3.left);
+
+        var angles = followTarget.transform.localEulerAngles;
+        angles.z = 0;
+
+        var angle = followTarget.transform.localEulerAngles.x;
+
+        if (angle > 180 && angle < 340)
+        {
+            angles.x = 340;
+        }
+        else if (angle < 180 && angle > 40)
+        {
+            angles.x = 40;
+        }
+
+        followTarget.transform.localEulerAngles = angles;
+
+        //rotate player based on look transform
+        transform.rotation = Quaternion.Euler(0, followTarget.transform.rotation.eulerAngles.y, 0);
+        followTarget.transform.localEulerAngles = new Vector3(angles.x, 0, 0);
+
+        //movement
+        if (playerController.isJumping) return;
+        if (!(inputVector.magnitude > 0)) moveDirection = Vector3.zero;
+
+        moveDirection = transform.forward * inputVector.y + transform.right * inputVector.x;
+        float currentSpeed = playerController.isRunning ? runSpeed : walkSpeed;
+
+        Vector3 movementDirection = moveDirection * (currentSpeed * Time.deltaTime);
+
+        transform.position += movementDirection;
+
+
+    }
+
+    public void OnMovement(InputValue value)
+    {
+        inputVector = value.Get<Vector2>();
+        playerAnimator.SetFloat(movementXHash, inputVector.x);
+        playerAnimator.SetFloat(movementYHash, inputVector.y);
+    }
+
+    public void OnRun(InputValue value)
+    {
+        playerController.isRunning = value.isPressed;
+        playerAnimator.SetBool(isRunningHash, playerController.isRunning);
+    }
+
+    public void OnJump(InputValue value)
+    {
+        if (playerController.isJumping)
+            return;
+
+        playerController.isJumping = value.isPressed;
+        rigidbody.AddForce((transform.up + moveDirection) * jumpForce, ForceMode.Impulse);
+        playerAnimator.SetBool(isJumpingHash, playerController.isJumping);
+    }
+
+    public void OnLook(InputValue value)
+    {
+        lookInput = value.Get<Vector2>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.gameObject.CompareTag("Ground") && !playerController.isJumping) return;
+
+        playerController.isJumping = false;
+        playerAnimator.SetBool(isJumpingHash, false);
+    }
+}
